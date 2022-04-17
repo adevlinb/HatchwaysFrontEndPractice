d74b313bea28c867b545b7c4e3c2d222c55ef9a5
adevlinb <93790582+adevlinb@users.noreply.github.com>
2022-04-16 23:45:02 -0700

    latest updates

--- src/App.js
+++ src/App.js
@@ -1,45 +1,37 @@
 import "./styles.css";
+import { useState } from 'react'
 import DisplayWeatherInfo from "./DisplayWeatherInfo/DisplayWeatherInfo"
 export default function App() {
+
+  let [weatherData, setWeatherData] = useState([])
 
   let key = process.env.weather_key;
   //coordinates for Hollywood
   let lat = 26.010669708251953;
   let lon = -80.16026306152344;
-  let days = ["Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"];
   
   
-  
+  // grab weather data / set to state
   async function getWeatherInfo(){
-    let today = new Date();
-    // let dayDate = today.substring(0,3);
-
-    // console.log(days[today.getDay() - 1], "hi")
-    //     days.forEach(function(day, ind){
-    //       if(day === "Sat") console.log(ind);
-    //     })
-      console.log(today)
-      console.log("hello")
       fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}&units=imperial`)
-      .then(res => {
-        console.log(res)
-        return res.json()
-      })
+      .then(res => res.json())
       .then((data) => {
-        return console.log(data, "again");
-      } )
+        setWeatherData([data.list[0], data.list[8], data.list[16], data.list[24], data.list[32]])
+      })
     }
 
+  // map weather data to component for rendering
+  let oneDayData = weatherData.map((data, index) => (
+    <DisplayWeatherInfo data={data} key={index}/>
+  ))
 
   return (
+
     <div className="App">
       <h1>Hatchways Front End Practice</h1>
-
-      <h1>Hi Im getting Info</h1>
       <button onClick={getWeatherInfo}>Get New Weather Info</button>
+      <div className="forecast">{oneDayData}</div>
       
-      
-      <DisplayWeatherInfo />
      
     </div>
   );
--- src/DisplayWeatherInfo/DisplayWeatherInfo.jsx
+++ src/DisplayWeatherInfo/DisplayWeatherInfo.jsx
@@ -1,10 +1,18 @@
+import "./DisplayWeatherInfo.css"
 
+export default function DisplayWeatherInfo({ data }) {
+ 
+  let today = new Date(data.dt_txt).toString().substring(0,3);
+  console.log(today, "hi")
 
-export default function DisplayWeatherInfo() {
   return (
-    <>
-      <h1>Weather INFO</h1>
-
-    </>
+    <div className="weatherData">
+      <h1>{today}</h1>
+      <div className="iconDiv" style={{ background: `url(http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png)`}}></div>
+      <div className="weatherTemps">
+        <h3>{data.main.temp_min.toString().substring(0,2)}°</h3>
+        <h3>{data.main.temp_max.toString().substring(0,2)}°</h3>
+      </div>
+    </div> 
   )
 }
